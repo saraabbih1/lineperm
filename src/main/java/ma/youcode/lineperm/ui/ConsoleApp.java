5997fec41d8c52f8da462dac7c4dfa20dfb1fab2
saraabbih1 <abbihsara30@gmail.com>
2026-09-10 09:10:14 +0100

    feat: singin login logout exit logic

--- src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
+++ src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
@@ -1,29 +1,125 @@
 package ma.youcode.lineperm.ui;
 import java.util.Scanner;
+import ma.youcode.lineperm.model.User;
 import ma.youcode.lineperm.service.UserService;
-import ma.youcode.lineperm.service.FileService;
+
 
 public class ConsoleApp {
 
+
     private UserService userService;
-     private FileService fileService;
     private Scanner scanner;
+    public boolean run ;
+    public User conectUser;
 
     public ConsoleApp() {
 
         userService = new UserService();
-           fileService = new FileService();
         scanner = new Scanner(System.in);
     }
 
     public void demarrer() {
+        run=true;
              userService.charger();
-              fileService.charger();
+
 
         System.out.println("LinePermission");
 
-        // boolean result = userService.createUser("sarra", "1234");
+        while(run) { 
+
+            printPrompt();
+            
+         String comnd=scanner.nextLine().trim().toLowerCase();
+switch(comnd){
+     case "singin":
+        singin();
+        break;
+        case "login":
+            login();
+            break;
+
+            case "logout":
+        Logout();
+        break;
+
+    case "exit":
+        run=false;
+        System.out.println("okeee by arras");
+        break;
+
+}
+            }
+    // boolean result = userService.createUser("sarra", "1234");
 
         // System.out.println("create sarra " + result);
     }
+    public void singin(){
+        if(conectUser!=null){
+            System.out.println("deja il ya un utilisateur conecter ");
+            return;
+        }
+        System.out.println("login:");
+        String login= scanner.nextLine();
+
+        System.out.println("password:");
+        String password = scanner.nextLine();
+
+        boolean newuser = userService.createUser(login, password);
+        if(newuser){
+            System.out.println("creat correct ");
+        }
+        else{
+            System.out.println("noooo");
+        }
+    }
+
+ public void login(){
+
+    if (conectUser != null) {
+        System.out.println(
+                "Une session est deja ouverte"
+        );
+        return;
+    }
+
+    System.out.print("Login : ");
+    String login = scanner.nextLine();
+
+    System.out.print("password: ");
+    String password = scanner.nextLine();
+
+    User user =
+            userService.authentification(login, password);
+
+    if (user == null) {
+        System.out.println("identifiants incorrects.");
+        return;
+    }  
+
+ }
+ private void printPrompt() {
+
+    if (conectUser == null) {
+        System.out.print("linperm> ");
+    } else {
+        System.out.print(
+                conectUser.getLogin() + "@linperm> "
+        );
+    }
 }
+
+private void Logout() {
+
+    if (conectUser == null) {
+        System.out.println(
+                "Aucun utilisateur connecte"
+        );
+        return;
+    }
+
+    conectUser = null;
+
+    System.out.println("Deconnexion reussie.");
+}
+}
+
