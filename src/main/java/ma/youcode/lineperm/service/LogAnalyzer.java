d8ff5ee960ea5393fcafbbb822456efbdf230a13
saraabbih1 <abbihsara30@gmail.com>
2026-09-17 16:51:40 +0100

    feat:LogAnalyzer/methode filter les logs par action

--- src/main/java/ma/youcode/lineperm/service/LogAnalyzer.java
+++ src/main/java/ma/youcode/lineperm/service/LogAnalyzer.java
@@ -1,6 +1,7 @@
 package ma.youcode.lineperm.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import ma.youcode.lineperm.model.LogEntry;
 
 public class LogAnalyzer {
@@ -15,5 +16,8 @@ public class LogAnalyzer {
     }
     public long nombreLongParUser(String user){
         return logs.stream().filter(log->log.getUser().equals(user)).count();
+    }
+    public List<LogEntry> logsParAction(String action){
+        return logs.stream().filter(log->log.getAction().equals(action)).collect(Collectors.toList());
     }
 }
