ff9e2dd60cbcbb54fa71b89cfccc99047570f1e6
saraabbih1 <abbihsara30@gmail.com>
2026-09-23 13:25:10 +0100

    feat:dao folder and db connection

--- src/main/java/ma/youcode/lineperm/database/TestDB.java
+++ src/main/java/ma/youcode/lineperm/database/TestDB.java
@@ -1,40 +1,14 @@
 package ma.youcode.lineperm.database;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class TestDB {
 
     public static void main(String[] args) throws Exception {
 
-        Connection connection =
-                DriverManager.getConnection("jdbc:sqlite:data/audit.db");
+        Connection connection = DBConnection.getConnection();
 
-        String sql = Files.readString(
-                Path.of("database/schema.sql")
-        );
-
-        Statement statement = connection.createStatement();
-
-        for (String request : sql.split(";")) {
-            if (!request.trim().isEmpty()) {
-                statement.executeUpdate(request);
-            }
-        }
-
-        ResultSet result = statement.executeQuery(
-                "SELECT name FROM sqlite_master WHERE type='table'"
-        );
-
-        System.out.println("Tables dans la base :");
-
-        while (result.next()) {
-            System.out.println(result.getString("name"));
-        }
+        System.out.println("Connexion SQLite réussie !");
 
         connection.close();
     }
