c428c786d2e534a7cc36a120f466bafdc890f858
saraabbih1 <abbihsara30@gmail.com>
2026-09-24 12:53:45 +0100

    feat: abstractDao

--- src/main/java/ma/youcode/lineperm/dao/AbstractDao.java
+++ src/main/java/ma/youcode/lineperm/dao/AbstractDao.java
@@ -1,5 +1,11 @@
 package ma.youcode.lineperm.dao;
+import java.sql.Connection;
+import ma.youcode.lineperm.database.*;
+public abstract class AbstractDao<T> implements Dao<T> {
 
-public class AbstractDao {
-    
+    protected Connection connection;
+
+    public AbstractDao() {
+        connection = DBConnection.getConnection();
+    }
 }
