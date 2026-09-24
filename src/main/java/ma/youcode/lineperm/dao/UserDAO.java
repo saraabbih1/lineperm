9f8faad7844a7cf96bbc42b4d58defb7aed1940a
saraabbih1 <abbihsara30@gmail.com>
2026-09-24 16:01:55 +0100

    feat:UserDAO: delet methode

--- src/main/java/ma/youcode/lineperm/dao/UserDAO.java
+++ src/main/java/ma/youcode/lineperm/dao/UserDAO.java
@@ -1,10 +1,11 @@
 package ma.youcode.lineperm.dao;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import ma.youcode.lineperm.model.User;
 
 public class UserDAO extends AbstractDao<User>{
+     @Override
     public void save(User user){
         String sql = "INSERT INTO users (login,password) VALUES(?,?)";
         try (PreparedStatement statement = connection.prepareStatement(sql)){
@@ -20,6 +21,47 @@ public class UserDAO extends AbstractDao<User>{
             e.printStackTrace();
         }
     }
-    
+    @Override
+public User findById(int id) {
 
+    String sql = "SELECT * FROM users WHERE id = ?";
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+
+        statement.setInt(1, id);
+
+        ResultSet result = statement.executeQuery();
+
+        if (result.next()) {
+
+            int userId = result.getInt("id");
+            String login = result.getString("login");
+            String password = result.getString("password");
+
+            return new User(userId, login, password);
+        }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    return null;
+}
+ @Override
+public void delete(int id) {
+
+    String sql = "DELETE FROM users WHERE id = ?";
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+
+        statement.setInt(1, id);
+
+        statement.executeUpdate();
+
+        System.out.println("Utilisateur supprime avec succes.");
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
 }
